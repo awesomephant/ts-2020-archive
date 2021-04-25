fa28854732890a892638afa99822b133c255f356
Max Kohler <max@koehler-kn.de>
2021-04-25 12:38:41 +0200

    minify html

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,17 @@
 const htmlmin = require("html-minifier");
 module.exports = function (eleventyConfig) {
+    eleventyConfig.addTransform("htmlmin", function (content, outputPath) {
+        if (outputPath.endsWith(".html")) {
+            let minified = htmlmin.minify(content, {
+                useShortDoctype: true,
+                removeComments: true,
+                collapseWhitespace: true
+            });
+            return minified;
+        }
 
+        return content;
+    });
     eleventyConfig.addShortcode("fig", function (url) {
         let height = 6
         if (url.includes('_S')) { height = 4 }
